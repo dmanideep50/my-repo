441070db68484030445dc08d7b8135f36603c4cd
dmanideep50 <dmanideep50@gmail.com>
2022-02-08 22:05:39 +0530

    project1

---
+++ src/com/project3/EmailValidation.java
@@ -0,0 +1,32 @@
+package com.project3;
+import java.util.ArrayList;
+//import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class EmailValidation {
+	public static boolean isValid(String email) {
+	String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+            "[a-zA-Z0-9_+&*-]+)*@" +
+            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+            "A-Z]{2,7}$";
+              
+Pattern pat = Pattern.compile(emailRegex);
+if (email == null)
+return false;
+return pat.matcher(email).matches();
+}
+	public static void main(String[] args) {
+		ArrayList<String> address = new ArrayList<>();
+        
+        address.add("dmanideep50@gmail.com");
+        address.add("abc.o@gmail.com");
+          
+      for(String i : address){
+          if (isValid(i))
+              System.out.println(i + " - Yes");
+          else
+              System.out.println(i + " - No");
+      }
+  }
+}
+
+
